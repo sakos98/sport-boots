ae89af1b60763c69d7520034a2f7864902052c5a
sakos98 <michusakowicz@gmail.com>
2023-12-06 16:39:31 +0100

    first commit

---
+++ src/main.ts
@@ -0,0 +1,25 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { LoggerInterceptor } from './shared/interceptors/logger.interceptor';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import * as path from 'path';
+
+async function bootstrap() {
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  app.useGlobalPipes(new ValidationPipe({ transform: true }));
+  app.useGlobalInterceptors(new LoggerInterceptor());
+  app.setGlobalPrefix('api');
+  app.enableCors();
+  app.enableShutdownHooks();
+
+  app.useStaticAssets(path.join(__dirname, '..', 'uploads'), {
+    prefix: '/uploads', 
+    index: false, 
+  });
+
+  await app.listen(8000);
+}
+bootstrap();
